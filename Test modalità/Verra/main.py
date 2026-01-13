5e24a62c6342b93b7759c7e39daaf445139539cf
Isa <isabel.ciartano@gmail.com>
2026-01-13 15:26:24 +0100

    update

--- Test modalità/Verra/main.py
+++ Test modalità/Verra/main.py
@@ -1,31 +1,41 @@
 from picamzero import Camera
 from exif import Image
+class ISSSpeedCalculator:
+    def take_picture(self):
+        #crea un istanza della classe camera
+        cam = Camera()
 
-def take_picture():
-    #crea un istanza della classe camera
-    cam = Camera()
+        i = 5
+        for cont in range(i):
+            filename = f"image{cont}.jpg"
+            cam.take_photo(filename)
+            sleep(10)
+            #cattura una sequenza di foto
+            #cam.capture_sequence("sequence",num_images=3, interval=3)
+        cam.take_photo("image1.jpg")
 
-    i = 5
-    for cont in range(i):
-        filename = f"image{cont}.jpg"
-        cam.take_photo(filename)
-        sleep(10)
-    #cattura una sequenza di foto
-    #cam.capture_sequence("sequence",num_images=3, interval=3)
-    cam.take_photo("image1.jpg")
+    def read_image(self):
+        i = 5
+        for cont in range(i):
+            img_file = open(f"image{i}.jpg","rb")#lettura in modalità binaria("rb")
+            lat = img.gps_latitude
+            lon = img.gps_longitude
+        return lat, lon
 
-def read_image():
-    i = 5
-    for cont in range(i):
-        img_file = open(f"image{i}.jpg","rb")#lettura in modalità binaria("rb")
-        lat = img.gps_latitude
-        lon = img.gps_longitude
-    return lat, lon
+    def scrivi_risultatiFinale(self,messaggio):#messaggio finale finale 
+        with open(self.file_risultati,'w')as f:
+            f.write(messaggio)
 
+    def esegui(self):
+
+        self.take_picture()
+        latitude, longitude = self.read_image()
+        self.scrivi_risultatiFinale(f"latitude = {latitude}       longitude = {longitude}")
+         
 def main():
-    take_picture()
-    latitude, longitude = read_image()
-    print(f"latitude = {latitude}       longitude = {longitude}")
+    calculator = ISSSpeedCalculator()
+    calculator.esegui()
+   
 
 if __name__  == "__main__": 
     main() 
